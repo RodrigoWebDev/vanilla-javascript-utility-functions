94918efec5a0deb3a4efe5f89e9e265878bc0046
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2020-10-27 14:18:03 -0300

    doubleKeyPressed

---
+++ utils.js
@@ -0,0 +1,28 @@
+function doubleKeyPress(key1, key2, callback) {
+  let key1State = false;
+  let key2State = false;
+
+  document.querySelector("body").addEventListener("keydown", function(e) {
+    toggleKeyState(e, true);
+
+    if (key1State === true && key2State === true) {
+      key1State = false;
+      key2State = false;
+      callback();
+    }
+  })
+
+  document.querySelector("body").addEventListener("keyup", function(e) {
+    toggleKeyState(e, false);
+  })
+
+  function toggleKeyState(e, state) {
+    if (e.keyCode === key1) {
+      key1State = state;
+    }
+
+    if (e.keyCode === key2) {
+      key2State = state;
+    }
+  }
+}
